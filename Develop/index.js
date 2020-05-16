3e1fb88ad17ce7a931b05d9cfdc15c5a7bc57049
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-05-16 17:53:49 -0500

    add questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,170 @@
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+
+// const generateMarkdown = (name, github) => `Name: ${name}, Github: ${github}`;
+// console.log(generateMarkdown(name, github));
+
+
+// NPM Modules
+const inquirer = require('inquirer');
+
 // array of questions for user
-const questions = [
+const questions = () => {
+    console.log(`
+    ===================================
+    Welcome to the README.md Generator!
+    ===================================
+    `);
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: '(Required) What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
 
-];
+        {
+            type: 'input',
+            name: 'github',
+            message: '(Required) Enter your GitHub Username:',
+            validate: githubUserInput => {
+                if (githubUserInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }    
+        },
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+        {
+            type: 'input',
+            name: 'title',
+            message: '(Required) What is title of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter the project title!');
+                    return false;
+                }
+            }
+        },
 
-// function to initialize program
-function init() {
+        {
+            type: 'input',
+            name: 'Repo',
+            message: '(Required) What is the link to the project repo?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter the URL for your project repo!');
+                    return false;
+                }
+            }
+        },
 
-}
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your e-mail address?'            
+        },
 
-// function call to initialize program
-init();
+        {
+            type: 'input',
+            name: 'describe',
+            message: '(Required) Enter a description of your project:',
+            validate: descInput => {
+                if (descInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter a description of your project!');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'languages',
+            message: 'Enter languages/technologies used:'       
+            // To be converted to checkbox with a list of languages/technologies at a later date
+            // choices: ['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'JQuery', 'Node.js']
+        },
+
+
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Enter installation instructions:'              
+        },
+
+        {
+            type: 'input',
+            name: 'useage',
+            message: 'Enter usage instructions:'              
+        },
+
+        {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select a license for your project:',
+        choices: ['GNU GPL v3', 'GNU GPL v2', 'MIT', 'BSD', 'Apache']
+        }, 
+
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Enter contribution instructions:'              
+        },
+
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter test instructions:'              
+        },
+
+        {
+            type: 'input',
+            name: 'ackowledge',
+            message: 'Enter acknowledgements:'              
+        },
+        
+    ]).then(answers => {
+        console.log(JSON.stringify(answers, null, '  '));
+    });
+};
+
+
+questions()
+
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+    
+
+// }
+
+// // function call to initialize program
+// init();
+
